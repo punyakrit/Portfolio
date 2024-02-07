6908a1526901bab2ec4098dd0fa743018dd0a235
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-02-08 00:46:54 +0530

    build

--- src/components/Par.tsx
+++ src/components/Par.tsx
@@ -1,22 +1,16 @@
-import React, { useCallback } from 'react'
+import  { useCallback } from 'react'
 import Particles from 'react-tsparticles'
 import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
-
+type Engine = any
+type Container = any
 function Par() {
 
     const particlesInit = useCallback(async (engine: Engine) => {
-        console.log(engine);
-
-        // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
-        //await loadFull(engine);
         await loadSlim(engine);
     }, []);
 
     const particlesLoaded = useCallback(async (container: Container | undefined) => {
-        await console.log(container);
     }, []);
   return (
     <div>
@@ -27,7 +21,7 @@ function Par() {
             options={{
                 "particles": {
                   "number": {
-                    "value": 80,
+                    "value": 21,
                     "density": {
                       "enable": false,
                       "value_area": 320.6824121731046
