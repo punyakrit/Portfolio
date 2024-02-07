f3a6a9020eaa952cc76c5feb10340cb1bd9d5076
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-02-08 00:29:39 +0530

    particles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 // import React from 'react'
 
+import MainSection from "./components/MainSection"
+import Par from "./components/Par"
+
 function App() {
   return (
-    <div className="bg-gray-800 w-screen h-screen text-white">
-      Website
+    <div className="bg-gray-900 w-screen h-screen text-white">
+      <Par/>
+      <MainSection/>
     </div>
   )
 }
---
+++ src/components/MainSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MainSection() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <div className='text-3xl'>
+      Hello My name is Punyakrit Singh Makhni
+
+        </div>
+    </div>
+  )
+}
+
+export default MainSection
